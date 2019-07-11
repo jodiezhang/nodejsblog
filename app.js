8de38d288e272d84b122828b9e15467319bec864
Judy Zhang <20080904@163.com>
2019-07-11 12:41:46 +0800

    处理POSTDATA

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
+
+//用于处理post data
+const getPostData = (req) => {
+   const promise = new Promise((resolve,reject) => {
+       if (req.method !== 'POST') {
+           resolve({})
+           return
+       }
+       if (req.headers['content-type'] !== 'application/json') {
+           resolve({})
+           return
+       }
+       let postData = ''
+       req.on('data',chunk => {
+           postData  += chunk.toString()
+       })
+       req.on('end', () => {
+           if(!postData){
+               resolve({})
+           }
+           resolve(
+               JSON.parse(postData)
+           ) 
+       })
+
+   })
+
+   return promise
+}
+
 const serverHandle = (req, res) =>{
 
     //设置返回格式 JSON
@@ -13,29 +43,33 @@ const serverHandle = (req, res) =>{
     //解析query
     req.query = querystring.parse(url.split('?')[0])
 
-
-    //处理blog路由
+    //处理post data
+    getPostData(req).then(postData => {
+        req.body = postData
+        //处理blog路由
     const blogData = handleBlogRouter(req,res)
     if (blogData) {
-        res.end(
-            JSON.stringify(blogData)
-        )
-        return
-    }
-    
+         res.end(
+             JSON.stringify(blogData)
+         )
+         return
+     } 
+     
     const userData = handleUserRouter(req,res)
-    if (userData) {
-        res.end(
-            JSON.stringify(userData)
-        )
-        return
-    }
-
-    //未命中路由，返回404
+     if (userData) {
+         res.end(
+             JSON.stringify(userData)
+         )
+         return
+     }
+ 
+     //未命中路由，返回404
     res.writeHead(404,{"Content-type":"text/plain"})
     res.write("404 Not Found\n")
     res.end()
- 
+
+    })
+
 }
 
 module.exports = serverHandle
