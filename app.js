bcb3c5441fafe322dd0680fae899a0c8f57df2e6
Judy Zhang <20080904@163.com>
2019-07-14 15:12:52 +0800

    连接数据库数据

--- app.js
+++ app.js
@@ -45,15 +45,19 @@ const serverHandle = (req, res) =>{
 
     //处理post data
     getPostData(req).then(postData => {
-        req.body = postData
+    req.body = postData
         //处理blog路由
-    const blogData = handleBlogRouter(req,res)
-    if (blogData) {
-         res.end(
-             JSON.stringify(blogData)
-         )
-         return
-     } 
+    const blogResult = handleBlogRouter(req,res)
+    if ( blogResult ) {
+        blogResult.then(blogData => {
+            res.end(
+                JSON.stringify(blogData)
+            )
+        })
+        return
+    }
+  
+
      
     const userData = handleUserRouter(req,res)
      if (userData) {
