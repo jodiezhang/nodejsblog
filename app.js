439430657518753ec1592bccdb20a98a6408908c
Judy Zhang <20080904@163.com>
2019-07-16 13:07:02 +0800

    使用cookie来实现登录但是会有安全问题

--- app.js
+++ app.js
@@ -43,6 +43,25 @@ const serverHandle = (req, res) =>{
     //解析query
     req.query = querystring.parse(url.split('?')[1])
 
+    //解析cookie
+    req.cookie = {}
+    const cookieStr = req.headers.cookie || ''
+    cookieStr.split(';').forEach(
+        item => {
+            if(!item) {
+                return
+            }
+            const arr = item.split('=')
+            const key = arr[0].trim()
+            const val = arr[1].trim()
+            // console.log(key)
+            // console.log(val)
+            req.cookie[key] = val
+        }
+    )
+
+    console.log('request cookie is '+req.cookie)
+
     //处理post data
     getPostData(req).then(postData => {
     req.body = postData
--- src/controller/user.js
+++ src/controller/user.js
@@ -1,14 +1,15 @@
 const { exec } = require('../db/mysql')
 
-const loginCheck = (username,password) => {
+const login = (username,password) => {
     const sql = `select username,realname from users where username = '${username}'
     and password='${password}'`
 
     return exec(sql).then(data => {
+        console.log(data)
         return data[0] || {}
     })
 }
 
 module.exports = {
-    loginCheck
+    login
 }
